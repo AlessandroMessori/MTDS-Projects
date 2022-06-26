62526989ebff1bd94a107c7cc2a2281311b6f2c4
AlessandroMessori <alessandro1.messori@mail.polimi.it>
2022-06-26 14:23:51 +0200

    added script to autogenerate node-red flows from config file

---
+++ SmartBuildings/node-red/generateFlows.py
@@ -0,0 +1,23 @@
+import json
+  
+# Opening JSON file
+f = open('./config.json')
+f2 = open("./Actuators.json")
+
+buildings = json.loads(f.read())
+flows = f2.read()
+
+for building in buildings:
+    for nfloor in range(building["nFloors"]):
+        for nroom in range(building["nRoomsPerFloor"]):
+           currentTopic = "building_{}/floor_{}/room_{}/sensors".format(building["buildingId"], nfloor, nroom+1)
+
+           currentFlow = flows.replace("iot/native/launchpad/json", currentTopic)
+
+           with open("generatedFlows/"+currentTopic.replace("/","_")+".json", "w") as outfile:
+            outfile.write(currentFlow)
+  
+# Closing file
+f.close()
+f2.close()
+print(flows)
