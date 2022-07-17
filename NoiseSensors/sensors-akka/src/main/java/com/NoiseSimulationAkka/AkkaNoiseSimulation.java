0e8ed02b3ceb97abb7fe85fc980f3653201a49e4
dariapreda <daria_maria.preda@stud.acs.upb.ro>
2022-07-17 13:19:54 +0200

    Increase no of sensor to 3; Real coordinates for sensor; Modify posX and posY to double; Increase action area; Added movement to sensors

--- NoiseSensors/sensors-akka/src/main/java/com/NoiseSimulationAkka/AkkaNoiseSimulation.java
+++ NoiseSensors/sensors-akka/src/main/java/com/NoiseSimulationAkka/AkkaNoiseSimulation.java
@@ -40,39 +40,64 @@ public class AkkaNoiseSimulation {
     int timeStep = 1000;
     int timeStamp = 0;
 
-    int posX = ThreadLocalRandom.current().nextInt(0, areaLength / 2);
-    int posY = ThreadLocalRandom.current().nextInt(0, areaWidth / 2);
+    //44.49379649240147, 11.343115957526301 Bologna
+    double posX1 = 44.4937;
+    double posY1 = 11.3431;
+    //44.51017702071235, 11.344914959441521 Bologna
+    double posX2 = 44.5101;
+    double posY2 = 11.3449;
+    //44.53257343793887, 11.288898485205035 Bologna
+    double posX3 = 44.5325;
+    double posY3 = 11.2888;
 
     final int QUEUE_SIZE = 6;
 
     final ActorSystem sys = ActorSystem.create("System");
-    /* TODO: Add multiple sensors
-    *  TODO: Randomize the positions better*/
-    final ActorRef simulatedSensor = sys.actorOf(Props.create(SensorObjectActor.class, posX, posY, QUEUE_SIZE,
-            maxNoiseLevelPeople, minNoiseLevelPeople, maxNoiseLevelVehicles, minNoiseLevelVehicles), "simulatedSensor");
 
+    final ActorRef simulatedSensor1 = sys.actorOf(Props.create(SensorObjectActor.class, posX1, posY1, QUEUE_SIZE,
+            maxNoiseLevelPeople, minNoiseLevelPeople, maxNoiseLevelVehicles, minNoiseLevelVehicles, 1), "simulatedSensor1");
+    final ActorRef simulatedSensor2 = sys.actorOf(Props.create(SensorObjectActor.class, posX2, posY2, QUEUE_SIZE,
+          maxNoiseLevelPeople, minNoiseLevelPeople, maxNoiseLevelVehicles, minNoiseLevelVehicles, 2), "simulatedSensor2");
+    final ActorRef simulatedSensor3 = sys.actorOf(Props.create(SensorObjectActor.class, posX3, posY3, QUEUE_SIZE,
+          maxNoiseLevelPeople, minNoiseLevelPeople, maxNoiseLevelVehicles, minNoiseLevelVehicles, 3), "simulatedSensor3");
     HashMap<String, ActorRef> simulatedPersons = new HashMap<String, ActorRef>();
     HashMap<String, ActorRef> simulatedVehicles = new HashMap<String, ActorRef>();
 
     for(int i = 0 ; i < 3; i++){
-      ActorRef newActor = sys.actorOf(Props.create(NoisePersonActor.class, posX + i, posY,
-              ThreadLocalRandom.current().nextDouble(1, 2)), "simulatedPerson" + i);
-      simulatedPersons.put("simulatedPerson" + i, newActor);
+      ActorRef newActor1 = sys.actorOf(Props.create(NoisePersonActor.class, posX1 + i * 0.001, posY1,
+              ThreadLocalRandom.current().nextDouble(1, 2)), "simulatedPerson" + 3*i + 0);
+      simulatedPersons.put("simulatedPerson" + 3*i + 0, newActor1);
+      ActorRef newActor2 = sys.actorOf(Props.create(NoisePersonActor.class, posX2 + i * 0.001, posY2,
+              ThreadLocalRandom.current().nextDouble(1, 2)), "simulatedPerson" + 3*i + 1);
+      simulatedPersons.put("simulatedPerson" + 3*i + 1, newActor2);
+      ActorRef newActor3 = sys.actorOf(Props.create(NoisePersonActor.class, posX3 + i * 0.001, posY3,
+              ThreadLocalRandom.current().nextDouble(1, 2)), "simulatedPerson" + 3*i + 2);
+      simulatedPersons.put("simulatedPerson" + 3*i + 2, newActor3);
     }
 
     for(int i = 0 ; i < 3; i++){
-      ActorRef newActor = sys.actorOf(Props.create(NoiseVehicleActor.class, posX + i, posY,
-              ThreadLocalRandom.current().nextDouble(1, 2)),"simulatedVehicle" + i);
-      simulatedVehicles.put("simulatedVehicle" + i, newActor);
+      ActorRef newActor1 = sys.actorOf(Props.create(NoiseVehicleActor.class, posX1 + i * 0.001, posY1,
+              ThreadLocalRandom.current().nextDouble(1, 2)),"simulatedVehicle" + 3*i + 0);
+      simulatedVehicles.put("simulatedVehicle" + 3*i + 0, newActor1);
+      ActorRef newActor2 = sys.actorOf(Props.create(NoiseVehicleActor.class, posX2 + i * 0.001, posY2,
+              ThreadLocalRandom.current().nextDouble(1, 2)),"simulatedVehicle" + 3*i + 1);
+      simulatedVehicles.put("simulatedVehicle" + 3*i + 1, newActor2);
+      ActorRef newActor3 = sys.actorOf(Props.create(NoiseVehicleActor.class, posX3 + i * 0.001, posY3,
+              ThreadLocalRandom.current().nextDouble(1, 2)),"simulatedVehicle" + 3*i + 2);
+      simulatedVehicles.put("simulatedVehicle" + 3*i + 2, newActor3);
     }
 
 
    while(true) {
       for (ActorRef actorP : simulatedPersons.values() ) {
-        actorP.tell(new RequestReadingMessage(posX, posY, timeStamp), simulatedSensor);
+        actorP.tell(new RequestReadingMessage(posX1, posY1, timeStamp), simulatedSensor1);
+        actorP.tell(new RequestReadingMessage(posX2, posY2, timeStamp), simulatedSensor2);
+        actorP.tell(new RequestReadingMessage(posX3, posY3, timeStamp), simulatedSensor3);
       }
       for (ActorRef actorV : simulatedVehicles.values()) {
-        actorV.tell(new RequestReadingMessage(posX, posY, timeStamp), simulatedSensor);
+        actorV.tell(new RequestReadingMessage(posX1, posY1, timeStamp), simulatedSensor1);
+        actorV.tell(new RequestReadingMessage(posX2, posY2, timeStamp), simulatedSensor2);
+        actorV.tell(new RequestReadingMessage(posX3, posY3, timeStamp), simulatedSensor3);
       }
       System.out.println("Checking all objects...");
 
