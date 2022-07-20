24b253e89a0176141ea8660711c88ada63c940d8
smntgl <simone.tagliente@mail.polimi.it>
2022-07-20 20:31:48 +0200

    cleaned repo

--- NoiseSensors/sensors-contiki/source/serial-communication/message_to_mote.js
+++
@@ -1,8 +0,0 @@
-/*This script sends a message to one mote with ID 1 every two seconds*/
-
-
-while(true){
-    GENERATE_MSG(10000, "sleep"); //Wait for two sec
-    YIELD_THEN_WAIT_UNTIL(msg.equals("sleep"));
-    write(sim.getMoteWithID(1), "some string");
-}
--- SmartBuildings/contiki/rpl-border-router/obj_native/assert.d
+++
@@ -1,6 +0,0 @@
-obj_native/assert.o: ../../os/lib/assert.c \
- ../../os/services/rpl-border-router/module-macros.h \
- ../../os/services/rpl-border-router/native/module-macros.h
-../../os/lib/assert.c :
- ../../os/services/rpl-border-router/module-macros.h :
- ../../os/services/rpl-border-router/native/module-macros.h :
